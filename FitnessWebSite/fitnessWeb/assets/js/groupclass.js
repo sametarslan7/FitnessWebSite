09e4e11275bcdecab14029b21f7e1bd87a5a840e
Samet Arslan <sametarslan1129@gmail.com>
2024-10-14 08:11:18 +0300

    GroupcClass - partialview changed
    GroupcClass - partialview changed

---
+++ FitnessWebSite/fitnessWeb/assets/js/groupclass.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    // İlk class gösterilecek, diğerleri gizlenecek
+    $('.class-content').hide();  // Tüm class içeriklerini gizliyoruz
+    $('.class-content').first().show();  // İlkini gösteriyoruz
+
+    // Class isimlerine tıklama olayını yönetiyoruz
+    $('.tab-link').click(function () {
+        var classId = $(this).data('id');  // Tıklanan class'ın ID'sini alıyoruz
+
+        // Tüm içerikleri gizle
+        $('.class-content').hide();
+
+        // Sadece tıklanan class'ın içeriğini göster
+        $('#class-content-' + classId).show();
+    });
+});
