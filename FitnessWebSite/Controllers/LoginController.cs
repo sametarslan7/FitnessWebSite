cace725739a412021745100df6c721fbb014f833
Samet Arslan <sametarslan1129@gmail.com>
2024-10-18 08:19:04 +0300

    added Sign In operations

---
+++ FitnessWebSite/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using FitnessWebSite.Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace FitnessWebSite.Controllers
+{
+    public class LoginController : Controller
+    {
+        Context c = new Context();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Admin ad)
+        {
+            var infos = c.Admins.FirstOrDefault(x => x.username == ad.username && x.password == ad.password);
+            if (infos != null)
+            {
+                FormsAuthentication.SetAuthCookie(infos.username, false);
+                Session["username"]=infos.username.ToString();
+                return RedirectToAction("Index","Admin");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
